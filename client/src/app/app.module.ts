07df8dba906750d91eb02b3bbb02beffd53ce948
Jakub Kováč <jakub.kovac@kosickaakademia.sk>
2023-03-22 23:58:12 +0100

    Added admin module Added Auth module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,36 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HeroHeaderComponent } from './hero-header/hero-header.component';
-import { NewsComponent } from './news/news.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HomeComponent} from './home/home.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {HeroHeaderComponent} from './hero-header/hero-header.component';
+import {NewsComponent} from './news/news.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {GalleryComponent} from './gallery/gallery.component';
+import {AdministrationModule} from "./modules/administration/administration.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NavbarComponent,
-    FooterComponent,
-    HeaderComponent,
-    HeroHeaderComponent,
-    NewsComponent,
-    AboutusComponent,
-    GalleryComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        NavbarComponent,
+        FooterComponent,
+        HeaderComponent,
+        HeroHeaderComponent,
+        NewsComponent,
+        AboutusComponent,
+        GalleryComponent,
+    ],
+    imports: [
+        AdministrationModule,
+        BrowserModule,
+        AppRoutingModule,
+        NoopAnimationsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ client/src/app/modules/administration/administration.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+
+@NgModule({
+  declarations: [
+    AdminPanelComponent
+  ],
+  exports: [
+    AdminPanelComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdministrationModule { }
