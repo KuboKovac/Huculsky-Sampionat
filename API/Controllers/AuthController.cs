52077d1b4acf6e111795399fa2a7e2b3eebeaf82
KuboKovac <jakub.kovac@kosickaakademia.sk>
2023-04-03 21:54:34 +0200

    Added new API controllers

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -3,17 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+
+// Controller responsible for Authorization and accounts management
+
+
 [ApiController]
 [Route("[controller]")]
 public class AuthController : ControllerBase
 {
-    [HttpPost("login")]
+    [HttpPost("Login")]
     public async Task<ActionResult<User>> Login()
     {
         return Ok("TODO");
     }
 
-    [HttpPost("register")]
+    [HttpPost("Register")]
     public async Task<ActionResult<User>> Register()
     {
         return Ok("TODO");
