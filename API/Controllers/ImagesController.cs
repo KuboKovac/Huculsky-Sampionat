fbbddfa240c265ff166b05950be4336f899f151f
KuboKovac <jakub.kovac@kosickaakademia.sk>
2023-04-03 21:59:28 +0200

    Added Images Controller

---
+++ API/Controllers/ImagesController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+// Controller for Adding/Removing Images
+
+[ApiController]
+[Route("[controller]")]
+public class ImagesController : ControllerBase
+{
+    [HttpGet("GetAllImages")]
+    public async Task<ActionResult> GetAllImages()
+    {
+        return Ok("TODO");
+    }
+}
