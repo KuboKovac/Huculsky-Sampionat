3cb1ee625799300867262a62601d9de6477e4775
KuboKovac <jakub.kovac@kosickaakademia.sk>
2023-05-28 20:59:28 +0200

    Articles: - Modified database, new migration - GetApprovedArticles - DeleteArticle - ModifyArticle - ApproveArticle - DisapproveArticle - Added schemas to endpoints that return data, see swagger

--- API/Controllers/ArticlesController.cs
+++ API/Controllers/ArticlesController.cs
@@ -22,14 +22,26 @@ public class ArticlesController : ControllerBase
     
     
     
-    [HttpGet("GetAllArticles")]
-    public async Task<ActionResult> GetAllArticles()
+    [HttpGet("GetAllArticles"), Authorize(Roles = "Admin,Editor")]
+    public async Task<ActionResult<List<Article>>> GetAllArticles()
     {
-        var articles = await _dbContext.Articles.Include(i => i.Images).ToListAsync();
+        var articles = await _dbContext.Articles.Include(i => i.Images)
+                                                          .Include(c => c.Comments)
+                                                          .ToListAsync();
         return Ok(articles);
     }
 
-    [HttpPost("CreateArticle"), Authorize(Roles = "Admin")]
+    [HttpGet("GetApprovedArticles")]
+    public async Task<ActionResult<List<Article>>> GetApprovedArticles()
+    {
+        var approvedArticles = await _dbContext.Articles.Where(approved => approved.IsApproved == true)
+            .Include(i => i.Images)
+            .Include(c => c.Comments)
+            .ToListAsync();
+        return Ok(approvedArticles);
+    }
+    
+    [HttpPost("CreateArticle"), Authorize(Roles = "Admin,Editor")]
     public async Task<ActionResult> CreateArticle(AddArticleDTO articleDto)
     {
         Article article = new Article();
@@ -37,7 +49,8 @@ public class ArticlesController : ControllerBase
         article.IsApproved = false;
         article.Name = articleDto.Name;
         article.Content = articleDto.Content;
-        article.Date = DateTime.Now;
+        article.CreatedBy = articleDto.CreatedBy;
+        article.Date = DateTime.Now.ToLocalTime();
 
         _dbContext.Articles.Add(article);
 
@@ -45,4 +58,70 @@ public class ArticlesController : ControllerBase
 
         return (Ok("Článok bol vytvorený!"));
     }
+    
+    [HttpDelete("DeleteArticle/{id:int}"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> DeleteArticle(int id)
+    {
+        var articleToDelete = await _dbContext.Articles.FindAsync(id);
+
+        if (articleToDelete == null)
+        {
+            return BadRequest("Článok nebol nájdený!");
+        }
+
+        await _dbContext.Articles.Where(article => article.Id == id).ExecuteDeleteAsync();
+        await _dbContext.SaveChangesAsync();
+        
+        return Ok("Článok bol vymazaný!");
+    }
+
+    [HttpPut("ModifyArticle/{id:int}"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> ModifyArticle(AddArticleDTO modifyDTO, int id)
+    {
+        var articleToModify = await _dbContext.Articles.FindAsync(id);
+
+        if (articleToModify == null)
+            return BadRequest("Článok sa nenašiel!");
+
+        articleToModify.Name = modifyDTO.Name;
+        articleToModify.Content = modifyDTO.Content;
+        articleToModify.CreatedBy = modifyDTO.CreatedBy;
+
+        _dbContext.Articles.Update(articleToModify);
+        await _dbContext.SaveChangesAsync();
+        
+        return Ok("Článok bol upravený!");
+    }
+
+    [HttpPut("ApproveArticle/{id:int}"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> ApproveArticle(int id)
+    {
+        var articleToApprove = await _dbContext.Articles.FindAsync(id);
+
+        if (articleToApprove == null) 
+            return BadRequest("Článok nebol nájdeny! Skontroluj ID");
+        
+        articleToApprove.IsApproved = true;
+        _dbContext.Articles.Update(articleToApprove);
+        await _dbContext.SaveChangesAsync();
+            
+        return Ok("Článok bol schváleny!");
+
+    }
+    [HttpPut("DisapproveArticle/{id:int}"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult> DisapproveArticle(int id)
+    {
+        var articleToApprove = await _dbContext.Articles.FindAsync(id);
+
+        if (articleToApprove == null) 
+            return BadRequest("Článok nebol nájdeny! Skontroluj ID");
+        
+        articleToApprove.IsApproved = false;
+        _dbContext.Articles.Update(articleToApprove);
+        await _dbContext.SaveChangesAsync();
+            
+        return Ok("Článok bol odstráneny spomedzi schválených článkov!");
+
+    }
+
 }
---
+++ API/Migrations/20230528173527_ArticleCreatedByProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArticleCreatedByProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Articles",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Articles");
+        }
+    }
+}
