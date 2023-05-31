1bebcb322f56c4d7b5e92b00bd6681addc3cc0a0
KuboKovac <jakub.kovac@kosickaakademia.sk>
2023-05-31 17:59:21 +0200

    Added endpoints for assing and unnasing horse from riders

--- API/Database/DbModels/Horse.cs
+++ API/Database/DbModels/Horse.cs
@@ -12,6 +12,8 @@ public class Horse
     public bool Male { get; set; }
     public int MotherId { get; set; }
     public int FatherId { get; set; }
+
+    public List<Rider> Riders { get; set; }
     public List<Result> Results { get; set; }
     public List<Competition> Competitions { get; set; }
     public List<Image> Images { get; set; }
