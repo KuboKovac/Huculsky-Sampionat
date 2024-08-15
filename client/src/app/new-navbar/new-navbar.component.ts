cca25cad9444118a39524b410b15dd9154c654ee
Matej Harabin <matejharabin@kosickaakademia.sk>
2024-08-16 01:33:48 +0200

    Fixed competetion result, fixed validators, added back button to homepage when you are on compoments page. Fixed navbar after click to close.

--- client/src/app/new-navbar/new-navbar.component.ts
+++ client/src/app/new-navbar/new-navbar.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-new-navbar',
   templateUrl: './new-navbar.component.html',
   styleUrls: ['./new-navbar.component.scss']
 })
+
 export class NewNavbarComponent {
   navbarOpened: Boolean = false;
   activeIndex: number | null = null;
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    if (this.navbarOpened && (event.target as HTMLElement).className == "dropdown-content-item") {
+      this.navbarOpened = false;
+    }
+  }
 
   navbarToggle() {
     this.navbarOpened = !this.navbarOpened;
